28e7319fddb212eea94a5235fd2fcb052ab46a9c
Oluwatobi-23534 <oluwatobi23534@gmail.com>
2023-11-29 03:32:21 +0100

    completed about page personal info and stats

--- src/data.jsx
+++ src/data.jsx
@@ -37,3 +37,92 @@ export const links = [
     path: "/contact",
   },
 ];
+
+export const personalInfo = [
+  {
+    id: 1,
+    title: "First Name : ",
+    description: "Oluwatobi",
+  },
+
+  {
+    id: 2,
+    title: "Last Name : ",
+    description: "Akinyemi",
+  },
+
+  {
+    id: 3,
+    title: "Age : ",
+    description: "26 Years",
+  },
+
+  {
+    id: 4,
+    title: 'Nationality : ',
+    description: 'Nigerian',
+  },
+
+  {
+    id: 5,
+    title: 'Freelance : ',
+    description: 'Available',
+  },
+
+  {
+    id: 6,
+    title: 'Address : ',
+    description: 'Lagos',
+  },
+
+  {
+    id: 7,
+    title: 'Phone : ',
+    description: '+2349162837696',
+  },
+
+  {
+    id: 8,
+    title: 'Email : ',
+    description: 'Oluwatobi23534@gmail.com',
+  },
+
+  {
+    id: 9,
+    title: 'Skype : ',
+    description: 'tobi.daniel',
+  },
+
+  {
+    id: 10,
+    title: 'Languages : ',
+    description: 'English',
+  },
+];
+
+export const stats = [
+  {
+    id: 1,
+    no: '2+',
+    title: 'Years of <br/> Experience'
+  },
+
+  {
+    id: 2,
+    no: '10+',
+    title: 'Completed <br/> projects'
+  },
+
+  {
+    id: 3,
+    no: '5+',
+    title: 'Happy <br/> Customers'
+  },
+
+  {
+    id: 4,
+    no: '2+',
+    title: 'Awards <br/> Won'
+  },
+
+]
--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,16 +1,44 @@
 /* eslint-disable no-unused-vars */
-import React from 'react'
+import React from "react";
+import Info from "../../components/Info";
+import Stats from "../../components/Stats";
+import { FaDownload } from "react-icons/fa";
+import CV from "../../assets/tobidaniel.pdf";
+import "./About.css"
 
 const About = () => {
   return (
-    <main className='section container'>
+    <main className="section container">
       <section className="about">
         <h2 className="section__title">
           About <span>Me</span>
         </h2>
-      </section>
-    </main>
-  )
-}
 
-export default About
+        <div className="about__container grid">
+          <div className="about__info">
+            <h3 className="section__subtitle">Personal Information</h3>
+
+            <ul className="info__list grid">
+              <Info />
+            </ul>
+
+            <a href={CV} download="" className="button">
+              Download CV{" "}
+              <span className="button__icon">
+                <FaDownload />
+              </span>
+            </a>
+          </div>
+
+          <div className="stats grid">
+            <Stats/>
+          </div>
+        </div>
+      </section>
+
+      <div className="separator"></div>
+    </main>
+  );
+};
+
+export default About;
